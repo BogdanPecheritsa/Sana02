b770853c65f0d8c8987e6dc24c351bed8c71d5c7
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2023-11-23 18:16:16 +0200

    Add project PointsAverage/

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+Console.WriteLine("Введіть 5 оцінок студента(0-100)");
+int score;
+int count = 0;
+int result = 0;
+while (count != 5)
+{
+    score = int.Parse(Console.ReadLine());
+    count++;
+    result += score / 5;
+}
+if (result >= 50)
+    Console.WriteLine("Студента допущено до екзамену");
+else
+{
+    Console.WriteLine("Студента не допущено до екзамену");
+}
