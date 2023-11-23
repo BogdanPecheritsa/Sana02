22ee3cec7215005778ab0516b33a9bfa1dd0efd9
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2023-11-23 18:10:29 +0200

    modified: QuadraticEquation/Program.cs

--- QuadraticEquation/Program.cs
+++ QuadraticEquation/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+double a, b, c, d, x1, x2;
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+Console.WriteLine("Значення а:");
+a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Значення b:");
+b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Значення c:");
+c = double.Parse(Console.ReadLine());
+
+if (a == 0)
+{
+    x1 = -c / b;
+    Console.WriteLine($"x={x1}");
+}
+else
+{
+    d = b * b - 4 * a * c;
+    if (d < 0)
+        Console.WriteLine("Коренів немає");
+    else if (d == 0)
+    {
+        x1 = -b / (2 * a);
+        Console.WriteLine($"x={x1}");
+    }
+    else if (d > 0)
+    {
+        x1 = (-b + System.Math.Sqrt(d)) / (2 * a);
+        x2 = (-b - System.Math.Sqrt(d)) / (2 * a);
+        Console.WriteLine($"x1={x1}, x2={x2}");
+    }
+}
