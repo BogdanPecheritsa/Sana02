8d897f11f5b992bfe2e93da591c92c821d6278e1
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2023-11-23 18:23:31 +0200

    modified: SwitchValues/Program.cs

--- SwitchValues/Program.cs
+++ SwitchValues/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+Console.WriteLine("Введіть число (0-9)");
+int choice;
+choice = int.Parse(Console.ReadLine());
+switch (choice)
+{
+    case 0: Console.WriteLine("Нуль"); break;
+    case 1: Console.WriteLine("Один"); break;
+    case 2: Console.WriteLine("Два"); break;
+    case 3: Console.WriteLine("Три"); break;
+    case 4: Console.WriteLine("Чотири"); break;
+    case 5: Console.WriteLine("П'ять"); break;
+    case 6: Console.WriteLine("Шість"); break;
+    case 7: Console.WriteLine("Сім"); break;
+    case 8: Console.WriteLine("Вісім"); break;
+    case 9: Console.WriteLine("Дев'ять"); break;
+    default: Console.WriteLine("Невірний діапазон чисел"); break;
+
+}
