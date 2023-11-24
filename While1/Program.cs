da62725e7014090d5cf92edab5996f7b27c183d2
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2023-11-24 13:51:06 +0200

    Modified While1/Program.cs

--- While1/Program.cs
+++ While1/Program.cs
@@ -1,47 +1,47 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
-double a = 0, b = 0, c = 0;
-int n1, n2, n3;
 Console.OutputEncoding = Encoding.Unicode;
 Console.InputEncoding = Encoding.Unicode;
 
-Console.WriteLine("Задайте к-сть ітерацій a");
-while (!int.TryParse(Console.ReadLine(), out n1) || n1 < 0)
-{
-    Console.WriteLine("Введіть додатне ціле число для к-сті ітерацій a:");
-}
+double a = 0, b = 0, c = 1;
 
-Console.WriteLine("Задайте к-сть ітерацій b");
-while (!int.TryParse(Console.ReadLine(), out n2) || n2 < 0)
-{
-    Console.WriteLine("Введіть додатне ціле число для к-сті ітерацій b:");
-}
+string[] vars = { "a", "b", "c" };
+int[] counts = new int[3];
 
-Console.WriteLine("Задайте к-сть ітерацій с");
-while (!int.TryParse(Console.ReadLine(), out n3) || n3 < 0)
+for (int i = 0; i < vars.Length; i++)
 {
-    Console.WriteLine("Введіть додатне ціле число для к-сті ітерацій c:");
-}
-
-for (int i = 0; i <= n1; i++)
-{
-    a += -1.0 / 3 + 1.0 / 5 + -1.0 * (i % 2 * 2 - 1) / (2 * i + 1);
-}
-Console.WriteLine($"a = {a}");
-
-for (int i = 1; i <= n2; i++)
-{
-    b += 1.0 / (i * i);
-}
-Console.WriteLine($"b = {b}");
-
-for (int i = 1; i <= n3; i++)
-{
-    int Fact = 1;
-    for (int j = 1; j <= i; j++)
+    Console.WriteLine($"Задайте к-сть ітерацій {vars[i]}");
+    while (!int.TryParse(Console.ReadLine(), out counts[i]) || counts[i] < 0)
     {
-        Fact *= j;
+        Console.WriteLine($"Введіть додатне ціле число для к-сті ітерацій {vars[i]}:");
     }
-    c += Fact;
 }
-Console.WriteLine($"c = {c}");
+
+for (int i = 0; i < counts.Length; i++)
+{
+    switch (vars[i])
+    {
+        case "a":
+            for (int j = 1; j <= counts[i]; j++)
+            {
+                a += 1.0 / (j * j);
+            }
+            Console.WriteLine($"a = {a}");
+            break;
+        case "b":
+            for (int j = 1; j <= counts[i]; j++)
+            {
+                b += 1.0 / (j * j);
+            }
+            Console.WriteLine($"b = {b}");
+            break;
+        case "c":
+            for (int j = 1; j <= counts[i]; j++)
+            {
+                c *= j;
+            }
+            Console.WriteLine($"c = {c}");
+            break;
+    }
+}
